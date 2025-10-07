f069bc13e4f9c8f68ef0241d5e460ed90ee4d2dc
nirmal <nirmalhota82@gmail.com>
2025-10-07 14:14:40 +0530

    Chat sample project

---
+++ ChatSample/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatSample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string name, int[] values )
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("ReceiveBatch", name, values);
+        }
+    }
+}
